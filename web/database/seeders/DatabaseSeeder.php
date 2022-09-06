d99be3c65ef8585b61cf14c95de5d68c48caf867
Makiiiiii <makiko.lucky1231@gmail.com>
2022-09-07 02:26:35 +0900

    #33 usageHistoryテーブル作成

--- web/database/seeders/DatabaseSeeder.php
+++ web/database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             ItemSeeder::class,
             UserSeeder::class,
+            UsageHistorySeeder::class,
         ]);
     }
 }
---
+++ web/database/seeders/UsageHistorySeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\UsageHistory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsageHistorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'user_id' => 1,
+                'item_id' => 1,
+                'is_returned' => true,
+                'start_at' => "2022-09-01",
+                'return_at' => "2022-09-10",
+            ],
+            [
+                'user_id' => 1,
+                'item_id' => 2,
+                'is_returned' => false,
+                'start_at' => "2022-09-03",
+                'return_at' => "2022-09-14",
+            ],
+            [
+                'user_id' => 1,
+                'item_id' => 3,
+                'is_returned' => false,
+                'start_at' => "2022-09-07",
+                'return_at' => "2022-09-14",
+            ],
+        ];
+
+        UsageHistory::insert($data);
+    }
+}
